1fb9bdb8084e475f82d0c703469051224aa72b06
eurolave <eurolave@gmail.com>
2025-09-30 22:42:48 +0300

    Delete src directory

--- src/core/nlu.js
+++
@@ -1,6 +0,0 @@
-export function detectIntent(text='') {
-  const t = text.trim();
-  if (/^[A-HJ-NPR-Z0-9]{17}$/i.test(t)) return 'VIN';
-  if (/^[A-Za-z0-9._-]{3,}$/.test(t)) return 'OEM';
-  return 'OTHER';
-}
